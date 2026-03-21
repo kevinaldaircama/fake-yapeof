270e3da21ac98f87390217a137859e415db812c6
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-03-20 21:15:52 -0500

    Update firebase-config.js

--- app/firebase-config.js
+++ app/firebase-config.js
@@ -1,9 +1,9 @@
 window.firebaseConfig = {
-  apiKey: "AIzaSyDVvEXCUiCPB5RmAVVWfmce2x-FALLDL0c",
-  authDomain: "roddoxes.firebaseapp.com",
-  databaseURL: "https://roddoxes-default-rtdb.firebaseio.com",
-  projectId: "roddoxes",
-  storageBucket: "roddoxes.firebasestorage.app",
-  messagingSenderId: "688697487472",
-  appId: "1:688697487472:web:7e3d19b907ae330dda04b5"
+  apiKey: "AIzaSyAgynl913FF8I1x23n1HZ5pYm6pS1p1i5M",
+  authDomain: "yape-b7dd2.firebaseapp.com",
+  databaseURL: "https://yape-b7dd2-default-rtdb.firebaseio.com",
+  projectId: "yape-b7dd2",
+  storageBucket: "yape-b7dd2.firebasestorage.app",
+  messagingSenderId: "921630422374",
+  appId: "1:921630422374:web:b622038a7b4128cc6284a7"
 };
