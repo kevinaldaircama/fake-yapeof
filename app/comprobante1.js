9aba807aefc2cf89f101e817de325ec0ecd92d1d
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-03-22 21:54:40 -0500

    Update comprobante1.js

--- app/comprobante1.js
+++ app/comprobante1.js
@@ -25,9 +25,10 @@ document.getElementById("nombre").textContent = nombre;
 const montoNum = parseFloat(monto);
 
 document.getElementById("monto").textContent =
-  montoNum % 1 === 0
+  "S/ " +
+  (montoNum % 1 === 0
     ? montoNum
-    : montoNum.toFixed(2);
+    : montoNum.toFixed(2));
 
 document.getElementById("telefono").textContent =
   `*** ***${telefono.slice(-3)}`;
