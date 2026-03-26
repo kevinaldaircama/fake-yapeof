c88ce1b9298ec792c8f96f519dba979a79cc16f9
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-03-25 19:23:39 -0500

    Update planes.js

--- app/planes.js
+++ app/planes.js
@@ -1,8 +1,41 @@
-function openModal(plan){
-  document.getElementById("modalTitle").innerText = plan;
-  document.getElementById("modal").style.display = "flex";
+async function pagar(plan, precio){
+
+try{
+
+let r = await fetch(
+"http://38.250.161.221:4906/crear_pago",
+{
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+plan:plan,
+precio:precio
+})
+
+});
+
+let data = await r.json();
+
+if(data.id){
+
+window.location =
+"https://www.mercadopago.com.pe/checkout/v1/redirect?pref_id="
++ data.id;
+
+}else{
+
+alert("Error creando pago");
+
 }
 
-function closeModal(){
-  document.getElementById("modal").style.display = "none";
+}catch(e){
+
+alert("Error al conectar con servidor");
+
+}
+
 }
